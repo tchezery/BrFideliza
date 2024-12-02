77924f33680e76c377d13dbdda4e16c132b00fc1
tchezery <159852972+tchezery@users.noreply.github.com>
2024-12-02 00:00:19 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,31 @@
+<?php 
+session_start();
+include('../connection.php');
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    $query = "SELECT * FROM participant WHERE email = ?";
+    $stmt = $mysqli -> prepare($query);
+    $stmt->bind_param("s", $email);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $participant = $result->fetch_assoc();
+        
+        if ($password === $participant['password']) {
+            $_SESSION['id_participant'] = $participant['id'];
+            $_SESSION['name'] = $participant['name']; 
+            header("Location: ../home/home.php");
+            exit;
+        } else {
+            header("Location: login.html");
+        }
+    } else {
+        header("Location: login.html");
+        exit;
+    }
+}
+?>
---
+++ subscribe.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    require_once '../connection.php';
+
+
+
+    $name = $_POST['name'];
+    $cpf = $_POST['cpf'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+
+    if (empty($name) || empty($cpf) || empty($email) || empty($phone) || empty($password)) {
+        die('Not completed');
+    }
+
+    $query = "INSERT INTO participant (name, cpf, email, phone, password) VALUES (?, ?, ?, ?, ?)";
+    $stmt = $mysqli->prepare($query);
+
+    if ($stmt === false) {
+        die('Erro in to prepare consult: ' . $mysqli->error);
+    }
+
+    $stmt->bind_param("sssss", $name, $cpf, $email, $phone, $password);
+
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        header("Location: ../login/login.html");
+        exit;
+    } else {
+        echo "Error " . $stmt->error;
+    }
+
+    $stmt->close();
+    $mysqli->close();
+
+}  else {
+    die('Not permited metody');
+}
+?>
